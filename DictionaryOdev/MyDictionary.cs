23e7cb71c438378cbf2310178603d950f7cea5f8
ANILFisting <anilbayir94@hotmail.com>
2021-02-01 18:26:00 +0200

    KampIntro

---
+++ DictionaryOdev/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryOdev
+{
+    class MyDictionary<TKey, TVal>
+    {
+        TKey[] keys;
+        TVal[] values;
+
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TVal[0];
+        }
+
+        public void Add(TKey key,TVal value)
+        {
+            TKey[] tempArrayK = keys;
+            keys = new TKey[keys.Length + 1];
+
+            TVal[] tempArrayV = values;
+            values = new TVal[values.Length + 1];
+
+            for (int i = 0; i < tempArrayK.Length; i++)
+            {
+                keys[i] = tempArrayK[i];
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempArrayV.Length; i++)
+            {
+                values[i] = tempArrayV[i];
+            }
+            values[values.Length - 1] = value;
+
+            Console.WriteLine("Id: " + key + " " + "İsim: " + value );
+        }
+
+    }
+}
---
+++ DictionaryOdev/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DictionaryOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1,"Anıl");
+            myDictionary.Add(2, "Mehmet");
+            myDictionary.Add(3,"Ahmet");
+
+            Console.ReadLine();
+        }
+    }
+}
